d8be81339512b5cee512afb62c7a2175f28bc1dd
Liane Heidemann <liane22070222@aluno.cesupa.br>
2026-05-15 01:41:36 -0300

    Update gestaoDeLoja.js

--- gestaoDeLoja.js
+++ gestaoDeLoja.js
@@ -1,145 +1,183 @@
-var continuar = true;
-var opcao;
-var produtoX;
-var valorX;
-var quantidadeX;
-var removerProduto;
+const readline = require("readline");
 
-//ESTOQUE DA LOJA
-var itensDoEstoque = [{
-    produto: "Roupas",
-    valor: 60,
-    quantidade: 46
-}, {
-    produto: "Calçados",
-    valor: 80,
-    quantidade: 30
-}, {
-    produto: "Acessórios",
-    valor: 80,
-    quantidade: 9
-}];
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-console.log("1 -> Exibir Estoque Completo\n2 -> Adicionar Item no Estoque\n3 - > Remover Produto do Estoque\n4 - > Editar produto\n5 - > Buscar Item\n6 -> Sair\n");
+// ================= ESTOQUE =================
+let itensDoEstoque = [
+    { produto: "Roupas", valor: 60, quantidade: 46 },
+    { produto: "Calçados", valor: 80, quantidade: 30 },
+    { produto: "Acessórios", valor: 80, quantidade: 9 }
+];
 
-//LISTA DE OPÇÕES 
-while (continuar) {
-    opcao = prompt("\nEscolha uma Opção: ");
-    switch (opcao) {
-        case "1":
-            printArray(itensDoEstoque);
-            break;
-        case "2":
-            addItemNoEstoque();
-            break;
-        case "3":
-            removerItemNoEstoque();
-            break;
-        case "4":
-            edicaoDeItens();
-            break;
-        case "5":
-            buscarItem();
-            break;
-        case "6":
-            console.log("\nSistema finalizado.");
-            continuar = false;
-            break;
-    }
-}
-
-function printArray(itensDoEstoque) {
-    for (var i = 0; i < itensDoEstoque.length; i++) {
-        console.log((i + 1) + ") - Produto: " + itensDoEstoque[i].produto + ", Preço: " + itensDoEstoque[i].valor +
-            ", Quantidade: " + itensDoEstoque[i].quantidade);
-    }
-}
-
-function addItemNoEstoque() {
-    produtoX = prompt("Nome do Produto: ");
-    valorX = prompt("Valor: ");
-    quantidadeX = prompt("Quantidade: ");
-    itensDoEstoque.push({
-        produto: produtoX,
-        valor: valorX,
-        quantidade: quantidadeX
+// ================= PROMPT ASSÍNCRONO =================
+function perguntar(texto) {
+    return new Promise(resolve => {
+        rl.question(texto, resposta => {
+            resolve(resposta);
+        });
     });
-    continuar = prompt("Adicionar outro Item? [S ou N] ");
-    if (continuar == "S" || continuar == "s") {
-        addItemNoEstoque();
+}
+
+// ================= MENU =================
+async function menu() {
+    let continuar = true;
+
+    console.log(`
+1 -> Exibir Estoque Completo
+2 -> Adicionar Item no Estoque
+3 -> Remover Produto do Estoque
+4 -> Editar Produto
+5 -> Buscar Item
+6 -> Sair
+`);
+
+    while (continuar) {
+        let opcao = await perguntar("\nEscolha uma opção: ");
+
+        switch (opcao) {
+            case "1":
+                printArray();
+                break;
+
+            case "2":
+                await addItemNoEstoque();
+                break;
+
+            case "3":
+                await removerItemNoEstoque();
+                break;
+
+            case "4":
+                await edicaoDeItens();
+                break;
+
+            case "5":
+                await buscarItem();
+                break;
+
+            case "6":
+                console.log("Sistema finalizado.");
+                continuar = false;
+                rl.close();
+                break;
+
+            default:
+                console.log("Opção inválida!");
+        }
     }
-    else {
+}
+
+// ================= FUNÇÕES =================
+
+function printArray() {
+    if (itensDoEstoque.length === 0) {
+        console.log("Estoque vazio.");
         return;
     }
-}
 
-function removerItemNoEstoque() {
-    removerProduto = prompt("Nome do Produto a ser Removido: ");
-    var i;
-    for (i = 0; i < itensDoEstoque.length; i++) {
-        if (itensDoEstoque[i].produto == removerProduto) {
-            break;
-        }
-    }
-    const idxObj = itensDoEstoque.findIndex(object => {
-        return object.produto === removerProduto;
+    itensDoEstoque.forEach((item, i) => {
+        console.log(
+            `${i + 1}) Produto: ${item.produto}, Preço: ${item.valor}, Quantidade: ${item.quantidade}`
+        );
     });
-    itensDoEstoque.splice(idxObj, i);
 }
 
-function edicaoDeItens() {
-    console.log("Nomes dos Produtos: Acessórios");;
-    nomeDoProduto = prompt("Nomes dos Produtos a serem Editados: ");
-    var i;
-    for (i = 0; i < itensDoEstoque.length; i++) {
-        if (itensDoEstoque[i].produto == nomeDoProduto) {
-            break;
-        }
+async function addItemNoEstoque() {
+    let produto = await perguntar("Nome do Produto: ");
+    let valor = await perguntar("Valor: ");
+    let quantidade = await perguntar("Quantidade: ");
+
+    itensDoEstoque.push({
+        produto,
+        valor: Number(valor),
+        quantidade: Number(quantidade)
+    });
+
+    let resposta = await perguntar("Adicionar outro item? [S/N]: ");
+
+    if (resposta.toLowerCase() === "s") {
+        await addItemNoEstoque();
     }
-    var editar = prompt("Selecione Opção: 1-> Nome 2->Valor, 3->Quantidade: ");
-    var dado = prompt("Novos Dados: ");
+}
+
+async function removerItemNoEstoque() {
+    let removerProduto = await perguntar("Nome do produto a ser removido: ");
+
+    const index = itensDoEstoque.findIndex(
+        item => item.produto === removerProduto
+    );
+
+    if (index !== -1) {
+        itensDoEstoque.splice(index, 1);
+        console.log("Produto removido com sucesso!");
+    } else {
+        console.log("Produto não encontrado.");
+    }
+}
+
+async function edicaoDeItens() {
+    printArray();
+
+    let nome = await perguntar("Nome do produto para editar: ");
+
+    let index = itensDoEstoque.findIndex(
+        item => item.produto === nome
+    );
+
+    if (index === -1) {
+        console.log("Produto não encontrado.");
+        return;
+    }
+
+    let editar = await perguntar("Editar: 1-> Nome | 2-> Valor | 3-> Quantidade: ");
+    let novoDado = await perguntar("Novo valor: ");
+
     switch (editar) {
         case "1":
-            itensDoEstoque[i].produto = dado;
+            itensDoEstoque[index].produto = novoDado;
             break;
+
         case "2":
-            itensDoEstoque[i].valor = dado;
+            itensDoEstoque[index].valor = Number(novoDado);
             break;
+
         case "3":
-            itensDoEstoque[i].quantidade = dado;
-            break
+            itensDoEstoque[index].quantidade = Number(novoDado);
+            break;
+
+        default:
+            console.log("Opção inválida.");
     }
 }
 
 function printItem(i) {
-    console.log((i + 1) + ") - Produto: " + itensDoEstoque[i].produto + ", Preço: " + itensDoEstoque[i].valor +
-        ", Quantidade: " + itensDoEstoque[i].quantidade);
+    let item = itensDoEstoque[i];
+    console.log(`${i + 1}) Produto: ${item.produto}, Preço: ${item.valor}, Quantidade: ${item.quantidade}`);
 }
 
-function buscarItem() {
-    var busca = prompt("Selecione Opção: 1-> Nome 2->Valor, 3->Quantidade: : ");
-    var filtro = prompt("Filtro : ");
-    switch (busca) {
-        case "1":
-            for (var i = 0; i < itensDoEstoque.length; i++) {
-                if (itensDoEstoque[i].produto == filtro) {
-                    printItem(i);
-                }
-            }
-            break;
-        case "2":
-            for (var i = 0; i < itensDoEstoque.length; i++) {
-                if (itensDoEstoque[i].valor == filtro) {
-                    printItem(i);
-                }
-            }
-            break;
-        case "3":
-            for (var i = 0; i < itensDoEstoque.length; i++) {
-                if (itensDoEstoque[i].quantidade == filtro) {
-                    printItem(i);
-                }
-            }
-            break;
-    }
+async function buscarItem() {
+    let tipo = await perguntar("Buscar por: 1-> Nome | 2-> Valor | 3-> Quantidade: ");
+    let filtro = await perguntar("Digite o filtro: ");
+
+    itensDoEstoque.forEach((item, i) => {
+        switch (tipo) {
+            case "1":
+                if (item.produto === filtro) printItem(i);
+                break;
+
+            case "2":
+                if (item.valor === Number(filtro)) printItem(i);
+                break;
+
+            case "3":
+                if (item.quantidade === Number(filtro)) printItem(i);
+                break;
+        }
+    });
 }
+
+// ================= INICIAR =================
+menu();
